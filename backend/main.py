3c2db2465361fdafc13d908f396d07d9edabe7a7
enzo <enzo.xr59@gmail.com>
2026-01-13 22:34:21 +0100

    iphone friendly video & images

--- backend/main.py
+++ backend/main.py
@@ -23,38 +23,32 @@ def init(data: dict):
     }
 products = {
     "stup": [
-        {
-            "id": 1,
-            "name": "STUP OG",
-            "price": "15€",
-            "video": "https://cdn.pixabay.com/video/2023/09/03/178307-861779302_large.mp4"
-        },
-        {
-            "id": 2,
-            "name": "STUP ICE",
-            "price": "20€",
-            "video": "https://cdn.pixabay.com/video/2023/08/31/177876-860787987_large.mp4"
-        }
+        {"id":1,"name":"STUP 1","price":"15€","video":"https://cdn.pixabay.com/video/2023/09/03/178307-861779302_large.mp4"},
+        {"id":2,"name":"STUP 2","price":"15€","video":"https://cdn.pixabay.com/video/2023/08/31/177876-860787987_large.mp4"},
+        {"id":3,"name":"STUP 3","price":"15€","video":"https://cdn.pixabay.com/video/2023/03/10/154331-807109997_large.mp4"},
+        {"id":4,"name":"STUP 4","price":"15€","video":"https://cdn.pixabay.com/video/2023/07/24/173568-847726095_large.mp4"},
+        {"id":5,"name":"STUP 5","price":"15€","video":"https://cdn.pixabay.com/video/2023/02/20/150621-800567532_large.mp4"},
+        {"id":6,"name":"STUP 6","price":"15€","video":"https://cdn.pixabay.com/video/2023/03/28/156841-812831195_large.mp4"},
+        {"id":7,"name":"STUP 7","price":"15€","video":"https://cdn.pixabay.com/video/2023/04/10/160789-820949911_large.mp4"},
+        {"id":8,"name":"STUP 8","price":"15€","video":"https://cdn.pixabay.com/video/2023/05/10/164209-828336023_large.mp4"},
+        {"id":9,"name":"STUP 9","price":"15€","video":"https://cdn.pixabay.com/video/2023/06/15/169852-842736441_large.mp4"},
+        {"id":10,"name":"STUP 10","price":"15€","video":"https://cdn.pixabay.com/video/2023/07/01/172543-846287764_large.mp4"},
+        {"id":11,"name":"STUP 11","price":"15€","video":"https://cdn.pixabay.com/video/2023/08/05/175432-853281554_large.mp4"},
+        {"id":12,"name":"STUP 12","price":"15€","video":"https://cdn.pixabay.com/video/2023/08/20/176543-857291143_large.mp4"},
+        {"id":13,"name":"STUP 13","price":"15€","video":"https://cdn.pixabay.com/video/2023/09/01/178001-860901233_large.mp4"}
     ],
     "puff": [
-        {
-            "id": 3,
-            "name": "PUFF BLUE",
-            "price": "10€",
-            "video": "https://cdn.pixabay.com/video/2023/03/10/154331-807109997_large.mp4"
-        }
+        {"id":20,"name":"PUFF BLUE","price":"10€","image":"https://picsum.photos/400/600?1"},
+        {"id":21,"name":"PUFF RED","price":"10€","image":"https://picsum.photos/400/600?2"},
+        {"id":22,"name":"PUFF GREEN","price":"10€","image":"https://picsum.photos/400/600?3"},
+        {"id":23,"name":"PUFF GOLD","price":"10€","image":"https://picsum.photos/400/600?4"},
+        {"id":24,"name":"PUFF BLACK","price":"10€","image":"https://picsum.photos/400/600?5"}
     ],
     "tabac": [
-    {
-        "id": 4,
-        "name": "TABAC GOLD",
-        "price": "12€",
-        "video": "https://cdn.pixabay.com/video/2023/07/24/173568-847726095_large.mp4"
-    }
-]
+        {"id":30,"name":"TABAC PREMIUM","price":"12€","video":"https://cdn.pixabay.com/video/2023/07/24/173568-847726095_large.mp4"}
+    ]
 }
 
 @app.get("/products/{cat}")
 def get_products(cat: str):
     return products.get(cat, [])
-
